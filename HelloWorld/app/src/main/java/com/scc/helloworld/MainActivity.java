9ef6f8d7ba7c51989603bdef12010b3fc331094a
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-30 14:57:11 +0600

    initial commit

---
+++ HelloWorld/app/src/main/java/com/scc/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.scc.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //https://www.youtube.com/watch?v=fis26HvvDII: 26.05
+    }
+}
