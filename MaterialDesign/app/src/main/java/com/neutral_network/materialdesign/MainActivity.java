4e6825b178d792c82e29b0215ff866836565703b
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-06 19:04:38 +0600

    Update MainActivity.java

--- MaterialDesign/app/src/main/java/com/neutral_network/materialdesign/MainActivity.java
+++ MaterialDesign/app/src/main/java/com/neutral_network/materialdesign/MainActivity.java
@@ -3,26 +3,12 @@ package com.neutral_network.materialdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MaterialCardView mcv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.material_cards);
-
-        mcv = findViewById(R.id.cardView);
-        mcv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Card clicked!", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
