55c0aea1a499db311fba14f6f96c36657a0ea3c7
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-06 21:37:08 +0600

    Update Contact.java

--- MaterialDesign/app/src/main/java/com/neutral_network/materialdesign/Contact.java
+++ MaterialDesign/app/src/main/java/com/neutral_network/materialdesign/Contact.java
@@ -1,4 +1,46 @@
 package com.neutral_network.materialdesign;
 
 public class Contact {
+    private String name;
+    private String email;
+    private String imageUrl;
+
+    public Contact(String name, String email, String imageUrl) {
+        this.name = name;
+        this.email = email;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
 }
