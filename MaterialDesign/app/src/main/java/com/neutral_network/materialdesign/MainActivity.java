98eab2aac6624d4622bb8403628056effd797558
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-08 00:00:35 +0600

    do some modification to looks good

--- MaterialDesign/app/src/main/java/com/neutral_network/materialdesign/MainActivity.java
+++ MaterialDesign/app/src/main/java/com/neutral_network/materialdesign/MainActivity.java
@@ -22,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<Contact> contacts = new ArrayList<>();
 
-        contacts.add(new Contact("Mike", "mike@freecodecamp.org", "https://static.vecteezy.com/system/resources/previews/005/151/717/original/cartoon-funny-bird-flying-isolated-on-white-background-free-vector.jpg"));
-        contacts.add(new Contact("Tom", "tom@freecodecamp.org", "https://www.wikihow.com/images/thumb/4/4b/1635699--27.JPG/-crop-375-300-375px-nowatermark-1635699--27.JPG"));
-        contacts.add(new Contact("Meisam", "meisam@freecodecamp.org", "https://easydrawingguides.com/wp-content/uploads/2017/04/how-to-draw-a-cartoon-boy-20.png"));
-        contacts.add(new Contact("Kelly", "kelly@freecodecamp.org", "https://cn.i.cdn.ti-platform.com/content/20/the-amazing-world-of-gumball/showpage/za/gumball-carousel.a94b8e60.png"));
-        contacts.add(new Contact("Jim", "jim@freecodecamp.org", "https://static.vecteezy.com/system/resources/previews/004/226/762/original/panda-cartoon-cute-say-hello-panda-animals-illustration-vector.jpg"));
+        contacts.add(new Contact("Mike", "mike@gmail.com", "https://static.vecteezy.com/system/resources/previews/005/151/717/original/cartoon-funny-bird-flying-isolated-on-white-background-free-vector.jpg"));
+        contacts.add(new Contact("Tom", "tom@gmail.com", "https://www.wikihow.com/images/thumb/4/4b/1635699--27.JPG/-crop-375-300-375px-nowatermark-1635699--27.JPG"));
+        contacts.add(new Contact("Meisam", "meisam@gmail.com", "https://easydrawingguides.com/wp-content/uploads/2017/04/how-to-draw-a-cartoon-boy-20.png"));
+        contacts.add(new Contact("Kelly", "kelly@gmail.com", "https://cn.i.cdn.ti-platform.com/content/20/the-amazing-world-of-gumball/showpage/za/gumball-carousel.a94b8e60.png"));
+        contacts.add(new Contact("Jim", "jim@gmail.com", "https://static.vecteezy.com/system/resources/previews/004/226/762/original/panda-cartoon-cute-say-hello-panda-animals-illustration-vector.jpg"));
 
 //        contacts.add(new Contact("Mike", "mike@freecodecamp.org", "https://static.vecteezy.com/system/resources/previews/005/151/717/original/cartoon-funny-bird-flying-isolated-on-white-background-free-vector.jpg"));
 //        contacts.add(new Contact("Tom", "tom@freecodecamp.org", "https://www.wikihow.com/images/thumb/4/4b/1635699--27.JPG/-crop-375-300-375px-nowatermark-1635699--27.JPG"));
