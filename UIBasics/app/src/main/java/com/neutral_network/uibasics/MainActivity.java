1f7a1099d71eae94d619dc414b71b7b3bbe03024
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-08 16:57:20 +0600

    add font

--- UIBasics/app/src/main/java/com/neutral_network/uibasics/MainActivity.java
+++ UIBasics/app/src/main/java/com/neutral_network/uibasics/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.different_xm_files_part2);
+        setContentView(R.layout.fonts);
     }
 }
