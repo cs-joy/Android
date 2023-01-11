4b1403bb1e9e50fb194d39bceb7eaaa7764a8064
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-12 05:41:20 +0600

    start to create the main logic for our application

--- UIChallange/app/src/main/java/com/neutral_network/uichallange/MainActivity.java
+++ UIChallange/app/src/main/java/com/neutral_network/uichallange/MainActivity.java
@@ -1,14 +1,59 @@
 package com.neutral_network.uichallange;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private EditText edtTxtName, edtTxtEmail, edtTxtPassword, edtTxtPassRepeat;
+    private Button btnPickImage, btnRegister;
+    private TextView txtWarnName, txtWarnEmail, txtWarnPass, txtWarnPassRepeat;
+    private Spinner spinnerCountry;
+    private RadioGroup rgGender;
+    private CheckBox agreementCheck;
+    private ConstraintLayout parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        Log.d(TAG, "initViews: Started");
+
+        edtTxtName = findViewById(R.id.edtTxtName);
+        edtTxtEmail = findViewById(R.id.edtTxtEmail);
+        edtTxtPassword = findViewById(R.id.edtTxtPassword);
+        edtTxtPassRepeat = findViewById(R.id.edtTxtPassRepeat);
+
+        btnPickImage = findViewById(R.id.btnPickImage);
+        btnRegister =findViewById(R.id.btnRegister);
+
+        txtWarnName = findViewById(R.id.txtWarnName);
+        txtWarnEmail = findViewById(R.id.txtWarnEmail);
+        txtWarnPass = findViewById(R.id.txtWarnPass);
+        txtWarnPassRepeat = findViewById(R.id.edtTxtPassRepeat);
+
+        spinnerCountry = findViewById(R.id.spinnerCountry);
+
+        rgGender = findViewById(R.id.rgGender);
+
+        agreementCheck = findViewById(R.id.agreementCheck);
+
+        parent = findViewById(R.id.parent);
     }
 }
