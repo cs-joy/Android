4a31ae6d288e507bc112ed963d582a1288ad1cfc
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-13 22:28:20 +0600

    chaquopy configured successfully with min sample

--- ChaquoPyJa/app/src/main/java/com/tss/chaquopyja/MainActivity.java
+++ ChaquoPyJa/app/src/main/java/com/tss/chaquopyja/MainActivity.java
@@ -3,12 +3,32 @@ package com.tss.chaquopyja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView helloTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloTxt = findViewById(R.id.txtView);
+
+        if (!Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+
+        Python py = Python.getInstance();
+        PyObject obj = py.getModule("hello");
+
+        PyObject atrObj = obj.callAttr("display");
+
+        helloTxt.setText(atrObj.toString());
     }
 }
---
+++ ChaquoPyJa/app/src/main/python/hello.py
@@ -0,0 +1,2 @@
+def display():
+    return "Hello World!"
