9376cc4570428ff370a41a9b3b27558a379565d5
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-14 15:08:05 +0600

    lol :alien:

--- ChaquoPyJa/app/src/main/java/com/tss/chaquopyja/MainActivity.java
+++ ChaquoPyJa/app/src/main/java/com/tss/chaquopyja/MainActivity.java
@@ -23,57 +23,38 @@ import java.io.ObjectOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edtTxtCX, edtTxtCY;
-    private Button btnPlot;
-    private ImageView displayPlot;
+    private EditText edtTxtFname, edtTxtLname;
+    private Button btnFullName;
+
+    private TextView displayGreet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtTxtCX = findViewById(R.id.edtTxtCX);
-        edtTxtCY = findViewById(R.id.edtTxtCY);
+        edtTxtFname = findViewById(R.id.edtTxtFname);
+        edtTxtLname = findViewById(R.id.edtTxtLname);
 
-        btnPlot = findViewById(R.id.btnPlot);
+        btnFullName = findViewById(R.id.btnFullName);
 
-        displayPlot = findViewById(R.id.imageView);
+        displayGreet = findViewById(R.id.displayGreet);
 
         if (!Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
 
         Python instance = Python.getInstance();
-        PyObject module = instance.getModule("plotting");
+        PyObject module = instance.getModule("concatenation");
 
-        btnPlot.setOnClickListener(new View.OnClickListener() {
+        btnFullName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    PyObject bytes = module.callAttr("plot", edtTxtCX, edtTxtCY);
-
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    try {
-                        ObjectOutputStream oos = new ObjectOutputStream(bos);
-                        oos.writeObject(bytes);
-                        oos.flush();
-                        byte[] data = bos.toByteArray();
-
-                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-
-                        displayPlot.setImageBitmap(bitmap);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                } catch (PyException e) {
-                    //Toast.makeText(this, "hi", Toast.LENGTH_LONG).show();
-                    System.out.println("error: " + e);
-                }
-
-
-
+                PyObject obj = module.callAttr("concatenate", edtTxtFname, edtTxtLname);
+                displayGreet.setText(obj.toString());
             }
         });
+
 
     }
 }
---
+++ ChaquoPyJa/app/src/main/python/concatenation.py
@@ -0,0 +1,5 @@
+
+def concatenate(fname, lname):
+    full_name = fname + " " + lname
+
+    return "full_name"
