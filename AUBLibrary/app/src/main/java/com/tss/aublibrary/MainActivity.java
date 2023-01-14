53811e20395b53fee0fc28c97baf78b3f3713f30
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-14 17:37:05 +0600

    initializing buttons

--- AUBLibrary/app/src/main/java/com/tss/aublibrary/MainActivity.java
+++ AUBLibrary/app/src/main/java/com/tss/aublibrary/MainActivity.java
@@ -3,8 +3,11 @@ package com.tss.aublibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnCurrentlyReading, btnAlreadyRead, btnWantToRead, btnFavorite, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
