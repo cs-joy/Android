00e7dc935ee2eb95e45b3b73b8cb02537c8aaabd
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-14 18:53:25 +0600

    initialize recycler view into our activity

--- AUBLibrary/app/src/main/java/com/tss/aublibrary/AllBooksActivity.java
+++ AUBLibrary/app/src/main/java/com/tss/aublibrary/AllBooksActivity.java
@@ -1,14 +1,19 @@
 package com.tss.aublibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class AllBooksActivity extends AppCompatActivity {
 
+    private RecyclerView booksRecView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        booksRecView = findViewById(R.id.booksRecView);
     }
 }
