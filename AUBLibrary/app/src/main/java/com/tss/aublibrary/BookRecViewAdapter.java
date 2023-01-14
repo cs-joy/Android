4ea94a2dbc59caafd1bfabd4a3192c0b0c382ac6
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-14 21:45:35 +0600

    Create BookRecViewAdapter.java

---
+++ AUBLibrary/app/src/main/java/com/tss/aublibrary/BookRecViewAdapter.java
@@ -0,0 +1,67 @@
+package com.tss.aublibrary;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class BookRecViewAdapter extends RecyclerView.Adapter<BookRecViewAdapter.ViewHolder> {
+
+    private static final String TAG = "BookRecViewAdapter";
+
+    private ArrayList<Book> books = new ArrayList<>();
+    private Context mContext;
+
+    public BookRecViewAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // create instance of our ViewHolder
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_book, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: Called");
+
+        holder.txtBookName.setText(books.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+        notifyDataSetChanged();// refresh the data in our RecyclerView
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView parent;
+        private ImageView imgBook;
+        private TextView txtBookName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // initializing those elements
+            parent = itemView.findViewById(R.id.parent);
+            imgBook = itemView.findViewById(R.id.imgBook);
+            txtBookName = itemView.findViewById(R.id.txtBookName);
+        }
+    }
+}
