14217f01962d9e9d2e1664a00d3bcd5cd9f1128f
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-14 18:40:16 +0600

    initialize a sample intent for a button to test effect of the new acitivity

--- AUBLibrary/app/src/main/java/com/tss/aublibrary/MainActivity.java
+++ AUBLibrary/app/src/main/java/com/tss/aublibrary/MainActivity.java
@@ -2,7 +2,9 @@ package com.tss.aublibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+
+        // click listener for btnAllBooks
+        btnAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // write here logic to navigate the user
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class); // Intent intent = new Intent(sourceActivity, destinationActivity);
+
+                // inner method
+                startActivity(intent);
+            }
+        });
     }
 
     private void initViews() {
