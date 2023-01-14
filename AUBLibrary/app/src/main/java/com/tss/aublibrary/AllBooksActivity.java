05c772b0f00d24a5a07fcd6471b8c1a125e0c1a2
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-14 22:14:51 +0600

    Update AllBooksActivity.java

--- AUBLibrary/app/src/main/java/com/tss/aublibrary/AllBooksActivity.java
+++ AUBLibrary/app/src/main/java/com/tss/aublibrary/AllBooksActivity.java
@@ -1,19 +1,32 @@
 package com.tss.aublibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
 
     private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
 
+        adapter = new BookRecViewAdapter(this);
         booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, "Operating System", "Andrew S. Tanenbaum", 765, "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Operating_Systems_Design_and_Implementation.jpg/220px-Operating_Systems_Design_and_Implementation.jpg", "Operating System: Design and Implementation is a computer science textbook written by Andrew S. Tanenbaum, with help from Albert S. Woodhull. ", "The book describes, in detail, the topic of Operating System Designing, It includes Tanenbaum's MINIX, a free Unix-like operating system designed for teaching purposes."));
+
+        adapter.setBooks(books);
     }
 }
