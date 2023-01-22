812d2c16ceb1577dd113f4411a22b880f0ac1d85
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-23 01:14:06 +0600

    u t

--- FaceDetection/app/src/main/java/com/tss/facedetection/MainActivity.java
+++ FaceDetection/app/src/main/java/com/tss/facedetection/MainActivity.java
@@ -2,27 +2,36 @@ package com.tss.facedetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
 
-    private Button btn;
-    private TextView txtView;
+public class MainActivity extends AppCompatActivity {
+    private Button btnDetect;
+    private ImageView imgVO;
+    private BitmapDrawable drawable;
+    private Bitmap bitmap;
+    private String imgString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnC);
-        txtView = findViewById(R.id.txtV);
+        btnDetect = findViewById(R.id.btnDetect);
+        imgVO = findViewById(R.id.imgVO);
 
         if (! Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
@@ -30,15 +39,34 @@ public class MainActivity extends AppCompatActivity {
 
         final Python pyInstance = Python.getInstance();
 
-        PyObject module = pyInstance.getModule("hello");
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnDetect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PyObject ob = module.callAttr("test");
-                txtView.setText(ob.toString());
+                drawable  = (BitmapDrawable)imgVO.getDrawable();
+                bitmap = drawable.getBitmap();
+                imgString = getStringImage(bitmap);
+
+                PyObject module = pyInstance.getModule("hello");
+
+                PyObject obj = module.callAttr("main", imgString);
+                String str = obj.toString();
+                byte data[] = android.util.Base64.decode(str, Base64.DEFAULT);
+                Bitmap bmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+                imgVO.setImageBitmap(bmap);
+
             }
         });
 
+    }
+
+    private String getStringImage(Bitmap bitmap) {
+        //
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = android.util.Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+        return encodedImage;
     }
 }
--- FaceDetection/app/src/main/python/hello.py
+++ FaceDetection/app/src/main/python/hello.py
@@ -1,2 +1,17 @@
-def test():
-    return 'hello world!'
+import numpy as np
+import cv2
+import PLI from Image
+import base64
+import io
+
+def main(imgString):
+    decodedData = base64.b64decode(imgString)
+    np_data = np.fromString(decodedData, np.uint8)
+    img = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+    img_grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    pil_img  = Image.fromarray(img_grayscale)
+    buff = io.BytesIO();
+    pil_img.save(buff, format="PNG")
+    img_str = base64.b64decode(buff.getvalue())
+
+    return "" + str(img_str, 'utf-8')
