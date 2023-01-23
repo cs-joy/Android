c82ac500b1c94c9cbee4dc228a6642d6616074bd
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-23 22:32:54 +0600

    small change to test

--- FaceDetection/app/src/main/java/com/tss/facedetection/MainActivity.java
+++ FaceDetection/app/src/main/java/com/tss/facedetection/MainActivity.java
@@ -10,7 +10,6 @@ import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
