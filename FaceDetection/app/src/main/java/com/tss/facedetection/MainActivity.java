14048cd0a07ba00150147a76c4f4ed70b50ac2fa
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-25 19:12:41 +0600

    won the game :100:

--- FaceDetection/app/src/main/java/com/tss/facedetection/MainActivity.java
+++ FaceDetection/app/src/main/java/com/tss/facedetection/MainActivity.java
@@ -1,11 +1,17 @@
 package com.tss.facedetection;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
@@ -16,21 +22,28 @@ import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnDetect;
-    private ImageView imgVO;
+    private Button btnDetect, BSelectImage;
+    private ImageView IVPreviewImage;
     private BitmapDrawable drawable;
-    private Bitmap bitmap;
+    private Bitmap bitmap, selectedImageBitmap;
     private String imgString = "";
+
+    private int SELECT_PICTURE = 200;
+
+    private Uri selectedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        selectImage();
+
         btnDetect = findViewById(R.id.btnDetect);
-        imgVO = findViewById(R.id.imgVO);
+        IVPreviewImage = findViewById(R.id.IVPreviewImage);
 
         if (! Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
@@ -41,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         btnDetect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawable  = (BitmapDrawable)imgVO.getDrawable();
+                drawable  = (BitmapDrawable)IVPreviewImage.getDrawable();
                 bitmap = drawable.getBitmap();
                 imgString = getStringImage(bitmap);
 
@@ -52,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 byte data[] = android.util.Base64.decode(str, Base64.DEFAULT);
                 Bitmap bmap = BitmapFactory.decodeByteArray(data, 0, data.length);
 
-                imgVO.setImageBitmap(bmap);
+                IVPreviewImage.setImageBitmap(bmap);
 
             }
         });
@@ -67,5 +80,77 @@ public class MainActivity extends AppCompatActivity {
         String encodedImage = android.util.Base64.encodeToString(imageBytes, Base64.DEFAULT);
 
         return encodedImage;
+    }
+
+    private void selectImage() {
+        // register the UI widgets with their appropriate IDs
+        BSelectImage = findViewById(R.id.BSelectImage);
+        IVPreviewImage = findViewById(R.id.IVPreviewImage);
+
+        // handle the Choose Image button to trigger
+        // the image chooser function
+        BSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+    }
+
+    private void imageChooser()
+    {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        launchSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null
+                            && data.getData() != null) {
+                        selectedImageUri = data.getData();
+
+                        try {
+                            selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        IVPreviewImage.setImageBitmap(
+                                selectedImageBitmap);
+                    }
+                }
+            });
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    IVPreviewImage.setImageURI(selectedImageUri);
+                }
+            }
+        }
     }
 }
