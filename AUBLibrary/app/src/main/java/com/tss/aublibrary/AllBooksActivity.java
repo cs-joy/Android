19fbe15af045069299e01470d79facd42ceb5c2f
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-05 09:30:16 +0600

    integrate with action bar only in this activity

--- AUBLibrary/app/src/main/java/com/tss/aublibrary/AllBooksActivity.java
+++ AUBLibrary/app/src/main/java/com/tss/aublibrary/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.tss.aublibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,8 @@ public class AllBooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         adapter = new BookRecViewAdapter(this, "allBooks");
         booksRecView = findViewById(R.id.booksRecView);
@@ -69,5 +73,17 @@ public class AllBooksActivity extends AppCompatActivity {
 
 //        adapter.setBooks(books);
           adapter.setBooks(Utils.getInstance().getAllBooks());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
