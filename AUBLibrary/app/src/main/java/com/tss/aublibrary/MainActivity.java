9c865c71e729fdc149f4ddd749fcf9bd5c4181f4
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-05 22:08:53 +0600

    pass context as parameter of getInstance method

--- AUBLibrary/app/src/main/java/com/tss/aublibrary/MainActivity.java
+++ AUBLibrary/app/src/main/java/com/tss/aublibrary/MainActivity.java
@@ -105,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Utils.getInstance();
+        Utils.getInstance(this);
     }
 
     private void initViews() {
